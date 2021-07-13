fb1bd177848d66af96de081139f58a277ad2bb87
Adi Murali <muraliadithya315@gmail.com>
2021-07-12 20:32:57 -0400

    Created WOLL

---
+++ concurrency/src/main/java/com/adi/concurrency/woll/WriteOnceLinkedList.java
@@ -0,0 +1,147 @@
+package com.adi.concurrency.woll;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * This implementation of List that provides same synchronization guarantees as CopyOnWriteArrayList,
+ * without creating a new array for each mutation of underlying array - thus allowing for memory efficient writes
+ *
+ * This class would be useful in scenarios where iterations outnumber writes and the objects being stored
+ * are large in size.
+ */
+public class WriteOnceLinkedList<E> implements List<E> {
+
+
+
+    public WriteOnceLinkedList() {
+
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    /**
+     * Iterates and removes the oldest values at a given position, for each position
+     * in the singly linked list
+     */
+    public void flushOldNodes() {
+
+    }
+}
